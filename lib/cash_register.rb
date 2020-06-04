f4ff35dc417c10d6f549a43296566201b24293ec
Bobby Neal <rsneal01@gmail.com>
2020-06-04 18:47:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
-
+class CashRegister
+  
+  attr_accessor :new, :total
+  
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def discount
+    self.new = 20
+  end
+  
+  def total
+    @total = 100
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    
+    # @total + price
+    # self.total = @total + price
+  end
+  
+end
