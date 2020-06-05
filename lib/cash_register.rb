2626601ca6f9d87df02b9605485ae52a3a007fc2
Bobby Neal <rsneal01@gmail.com>
2020-06-05 18:15:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,9 @@ class CashRegister
 
   def add_item(item, price, quantity = 1)
     self.total += price * quantity
-    @items << 
+    quantity.times.do 
+      @items << item
+    end
   end
   
   def apply_discount
